457c0ca3df7d293095f51a2d3f9a1d70ca955614
User <sv.alexey00@mail.ru>
2022-11-17 13:22:49 +0300

    аап

--- apppi/fdb.py
+++ apppi/fdb.py
@@ -12,6 +12,27 @@ def create_db():
     db = connect_db()
     with app.open_resource('sql_db.sql', mode='r') as f:
         db.cursor().executescript(f.read())
-        db.commit()
-        db.close()
-        pass
+    db.commit()
+    db.close()
+    pass
+class FDataBase:
+    def  __init__(self, db):
+        self.__db = db
+        self.__cur = db.cursor()
+
+    def delMenu(self, id=0):
+        try:
+            if id == 0:
+                self.__cur.execute('DELETE FROM mainmenu')
+            else:
+                pass
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print('Ошибка добавления в БД', e)
+            return False
+        return True
+if __name__== '__main__':
+    db = connect_db()
+    db = FDataBase(db)
+    #print(db.setMenuMenu('Разработчик', 'about'))
+    print(db.delMenu())
